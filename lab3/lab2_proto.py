dbd0756ddcf30d248749579efbe078f838d996b8
Axel <axelsparr@hotmail.com>
2024-05-04 13:24:05 +0200

    setup för lab 3

---
+++ lab3/lab2_proto.py
@@ -0,0 +1,200 @@
+import numpy as np
+
+
+def concatTwoHMMs(hmm1, hmm2):
+   """ Concatenates 2 HMM models
+
+    Args:
+       hmm1, hmm2: two dictionaries with the following keys:
+           name: phonetic or word symbol corresponding to the model
+           startprob: M+1 array with priori probability of state
+           transmat: (M+1)x(M+1) transition matrix
+           means: MxD array of mean vectors
+           covars: MxD array of variances
+
+    D is the dimension of the feature vectors
+    M is the number of emitting states in each HMM model (could be different for each)
+
+    Output
+       dictionary with the same keys as the input but concatenated models:
+          startprob: K+1 array with priori probability of state
+          transmat: (K+1)x(K+1) transition matrix
+             means: KxD array of mean vectors
+            covars: KxD array of variances
+
+    K is the sum of the number of emitting states from the input models
+   
+    Example:
+       twoHMMs = concatHMMs(phoneHMMs['sil'], phoneHMMs['ow'])
+    """
+   try:
+     hmm1["means"]
+   except:
+       print(hmm1)
+   #startprob
+   M1 = len(hmm1["startprob"])
+   M2 = len(hmm2["startprob"])
+   startprob_conc = np.zeros((M1+M2-1))
+   for i in range(M1+M2-1):
+      if i<M1-1:
+         startprob_conc[i]=hmm1["startprob"][i]
+      else:
+         startprob_conc[i]=hmm1["startprob"][-1]*hmm2["startprob"][i-M1]
+
+         
+   #transmat
+   A = hmm1["transmat"][:-1, :-1] #n-1 x n-1 matrix, skip last row and column
+   B1 = hmm1["transmat"][:-1, -1]  #last row minus the 1 at the bottom in first mat.
+   B2 = hmm2["startprob"]
+   B = B1.reshape(-1, 1) @ B2.reshape(1, -1) #reshape to go from (3,) x (4,) to (3,1) x (1,4)
+   C = np.zeros((M2,M1-1))
+   D = hmm2["transmat"]
+   top_row = np.concatenate((A, B), axis=1)
+   bottom_row = np.concatenate((C, D), axis=1)
+   transmat_conc = np.concatenate((top_row, bottom_row), axis=0)
+
+   #mean
+   top = hmm1["means"]
+   bottom = hmm2["means"]
+   mean_conc = np.concatenate((top, bottom), axis=0)
+
+   #covars
+   top = hmm1["covars"]
+   bottom = hmm2["covars"]
+   covars_conc = np.concatenate((top, bottom), axis=0)
+
+   return {"startprob": startprob_conc,
+           "transmat": transmat_conc,
+           "means": mean_conc,
+           "covars": covars_conc,
+           "name": hmm1["name"] +"_"+hmm2["name"]
+           }
+
+# this is already implemented, but based on concat2HMMs() above
+def concatHMMs(hmmmodels, namelist):
+    """ Concatenates HMM models in a left to right manner
+
+    Args:
+       hmmmodels: dictionary of models indexed by model name. 
+       hmmmodels[name] is a dictionaries with the following keys:
+           name: phonetic or word symbol corresponding to the model
+           startprob: M+1 array with priori probability of state
+           transmat: (M+1)x(M+1) transition matrix
+           means: MxD array of mean vectors
+           covars: MxD array of variances
+       namelist: list of model names that we want to concatenate
+
+    D is the dimension of the feature vectors
+    M is the number of emitting states in each HMM model (could be
+      different in each model)
+
+    Output
+       combinedhmm: dictionary with the same keys as the input but
+                    combined models:
+         startprob: K+1 array with priori probability of state
+          transmat: (K+1)x(K+1) transition matrix
+             means: KxD array of mean vectors
+            covars: KxD array of variances
+
+    K is the sum of the number of emitting states from the input models
+
+    Example:
+       wordHMMs['o'] = concatHMMs(phoneHMMs, ['sil', 'ow', 'sil'])
+    """
+    concat = hmmmodels[namelist[0]]
+    for idx in range(1,len(namelist)):
+        concat = concatTwoHMMs(concat, hmmmodels[namelist[idx]])
+    return concat
+
+
+def gmmloglik(log_emlik, weights):
+    """Log Likelihood for a GMM model based on Multivariate Normal Distribution.
+
+    Args:
+        log_emlik: array like, shape (N, K).
+            contains the log likelihoods for each of N observations and
+            each of K distributions
+        weights:   weight vector for the K components in the mixture
+
+    Output:
+        gmmloglik: scalar, log likelihood of data given the GMM model.
+    """
+
+def forward(log_emlik, log_startprob, log_transmat):
+    """Forward (alpha) probabilities in log domain.
+
+    Args:
+        log_emlik: NxM array of emission log likelihoods, N frames, M states
+        log_startprob: log probability to start in state i
+        log_transmat: log transition probability from state i to j
+
+    Output:
+        forward_prob: NxM array of forward log probabilities for each of the M states in the model
+    """
+    N,M = log_emlik.shape()
+
+    alpha = np.zeros(N,M)
+    alpha[0,:] = log_startprob[:-1]+log_emlik[0,:] #inte helt 100 
+
+    for i in range(1,N):
+        for j in range(1,M):
+            alpha[i,j] = np.sum(alpha[i-1,j]*log_transmat[i,j])*log_emlik[i,j] #inte alls 100
+            
+    
+    
+
+
+def backward(log_emlik, log_startprob, log_transmat):
+    """Backward (beta) probabilities in log domain.
+
+    Args:
+        log_emlik: NxM array of emission log likelihoods, N frames, M states
+        log_startprob: log probability to start in state i
+        log_transmat: transition log probability from state i to j
+
+    Output:
+        backward_prob: NxM array of backward log probabilities for each of the M states in the model
+    """
+
+def viterbi(log_emlik, log_startprob, log_transmat, forceFinalState=True):
+    """Viterbi path.
+
+    Args:
+        log_emlik: NxM array of emission log likelihoods, N frames, M states
+        log_startprob: log probability to start in state i
+        log_transmat: transition log probability from state i to j
+        forceFinalState: if True, start backtracking from the final state in
+                  the model, instead of the best state at the last time step
+
+    Output:
+        viterbi_loglik: log likelihood of the best path
+        viterbi_path: best path
+    """
+
+def statePosteriors(log_alpha, log_beta):
+    """State posterior (gamma) probabilities in log domain.
+
+    Args:
+        log_alpha: NxM array of log forward (alpha) probabilities
+        log_beta: NxM array of log backward (beta) probabilities
+    where N is the number of frames, and M the number of states
+
+    Output:
+        log_gamma: NxM array of gamma probabilities for each of the M states in the model
+    """
+
+def updateMeanAndVar(X, log_gamma, varianceFloor=5.0):
+    """ Update Gaussian parameters with diagonal covariance
+
+    Args:
+         X: NxD array of feature vectors
+         log_gamma: NxM state posterior probabilities in log domain
+         varianceFloor: minimum allowed variance scalar
+    were N is the lenght of the observation sequence, D is the
+    dimensionality of the feature vectors and M is the number of
+    states in the model
+
+    Outputs:
+         means: MxD mean vectors for each state
+         covars: MxD covariance (variance) vectors for each state
+    """
